45ef5e47432da7d1e82776997350a40eeb913303
Ooi Wei Gen <s10910627@thu.edu.tw>
2025-02-24 15:52:32 +0800

    added dplyr library

--- Test_shiny.R
+++ Test_shiny.R
@@ -1,6 +1,1 @@
-library(shiny)
-ui <- fluidPage(plotOutput("plot"))
-server <- function(input, output) {
-  output$plot <- renderPlot({ plot(mtcars$mpg, mtcars$wt) })
-}
-shinyApp(ui, server)
+> plot(1:10)
